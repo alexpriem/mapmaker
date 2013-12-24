f5db7bb9bc6db15a0f1455a8b8d26c236f7ee856
Alex Priem <alexpriem@gmail.com>
2013-12-24 15:17:39 +0100

    remove testcode

--- mapmaker.py
+++ mapmaker.py
@@ -5,6 +5,10 @@ from matplotlib import pyplot
 import sys
 
 
+
+
+
+
 def drawpolygon(p,graph):
     #print type(p)
     
@@ -26,47 +30,29 @@ def drawpolygon(p,graph):
                         color=bordercolor,
                         linewidth=borderwidth)
 
+
+filename="wp_2012s.shp"
+data="polen_2013.csv"
 fig = pyplot.figure(figsize=(7, 8),dpi=300)
 ax = fig.add_subplot(111)
 
-#EPSG:28992
-#epsg:3857 
-
-
-source = osr.SpatialReference()
-source.ImportFromEPSG(28992)
-
-target = osr.SpatialReference()
-target.ImportFromEPSG(3857)
-transform = osr.CoordinateTransformation(source, target)
 
 print 'reading data'
-sh = ogr.Open("f:\\data\\maps\\shapefiles\\gm_2012.shp")
+sh = ogr.Open("f:\\data\\maps\\shapefiles\\"+filename)
 driver = ogr.GetDriverByName('ESRI Shapefile')
 layer = sh.GetLayer()
-print layer.GetName()
-defn=layer.GetLayerDefn()
-print defn.GetName()
-
-
 
 for feature in layer:
     print feature.GetFieldCount()
     print feature.GetField("GM_NAAM")
     print feature.DumpReadable()
-   # print feature.GetFieldDefnRef(1)
     sys.exit()
-    #geom = feature.GetGeometryRef()
-    #print geom.Centroid().ExportToWkt()    
     geom=feature.GetGeometryRef()
     if geom is not None:    
         geometryParcel = loads(geom.ExportToWkb())
-       # newgeo=ogr.CreateGeometryFromWkt(geometryParcel)
-      #  newgeo.Transform(transform)
         drawpolygon(geometryParcel , ax)
 print 'saving img'
 pyplot.savefig('datafile.png')
-pyplot.savefig('datafile.svg')    
 
 
 
