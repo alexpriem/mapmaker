ebc7abbc50b45080cd8f391e3dd7a5d249851714
Alex Priem <alexpriem@gmail.com>
2014-01-13 15:14:12 +0100

    tijdreeksen aan map toegevoegd.

--- js/ui.js
+++ js/ui.js
@@ -1,69 +1,303 @@
+var regio_ts={};
+var date2date={};
+var daysel=0;
+var regiosel=0;
+var varsel=varnames[2];
 
+var minval=0;
+var maxval=0;
+var tminval=0;
+var tmaxval=0;
+
+var color=[];
+var canvas;
+
+ts_width=800;
+ts_height=200;
 
 function click_regio() {
 
+	$('#svg_ts').remove();
 	console.log(this.id);	
+	r=this.id.split('_')[0];
+	regiosel=r.slice(1);
+	console.log(regiosel);
+	update_ts();
+	return false;
+}
+
+
+function update_var_info ()  {
+
+	varidx=varnames.indexOf(varsel);
+	minval=var_min[varidx];
+	tminval=minval;
+	if (minval!=0) tminval=Math.log(minval);	
+	maxval=var_max[varidx];	// FIXME: transform bijhouden.
+	tmaxval=minval;
+	if (maxval!=0) tmaxval=Math.log(maxval);
 }
 
 function change_var () {
 
 /* todo: cleanup  + handle multipolygons */
 
+	
+	var varsel=this.id.split('_')[1];
+	console.log(varsel);
+	update_var_info ();
+	update_choropleth();	
+	return false;
+}
 
 
-	var varname=this.id.split('_')[1];
-	var varidx=this.id.split('_')[2];
-	console.log(varname);
-	minval=var_min[varidx];
-	maxval=Math.log(var_max[varidx]);
-	console.log(data);	
-	for (var key in data) {
-		if (data.hasOwnProperty(key)){
-			var regioinfo=data[key];
-			//console.log(regioinfo);
-			val=regioinfo[varidx-1];
-		//	console.log(val);
+function prep_data () {
+
+	var records=data.length;
+	var row, ts,regio;
+	
+
+	for (i=0; i<records; i++) {
+		row=data[i];
+		d=row[0];
+		regio=row[1];
+
+		year=parseInt(d/10000.0);
+		month=parseInt((d-year*10000)/100);
+		day=parseInt(d-year*10000-month*100);		
+		ddate= new Date(year,month-1,day); 
+		date2date[d]= ddate;
+
+		if (!(regio in regio_ts)) {
+			regio_ts[regio]=[[ddate,row[2]]];
+		} else {
+			regio_ts[regio].push([ddate,row[2]]);
+		}
+	}
+
+	min_d=var_min[0];
+	year=parseInt(min_d/10000.0);
+	month=parseInt((min_d-year*10000)/100);
+	day=parseInt(min_d-year*10000-month*100);	
+	mindate=new Date(year,month-1,day); 
+
+	max_d=var_max[0];
+	year=parseInt(max_d/10000.0);
+	month=parseInt((max_d-year*10000)/100);
+	day=parseInt(max_d-year*10000-month*100);	
+	maxdate=new Date(year,month-1,day); 
+
+	daysel=data[0][0];
+	regiosel=data[0][1];
+}
+
+
+function update_choropleth () {
+
+	var records, color, s;
+
+
+	update_var_info();   // 
+	console.log("day/regio/var",daysel,regiosel,varsel);
+	records=data.length;
+	for (rownr=0; rownr<records; rownr++){	
+		row=data[rownr];
+		if (row[0]==daysel) {						
+			var regio=row[1];			
+			val=row[varidx];			
+		//	console.log(regio, val);
 			if (val!=0){
 				val=Math.log(val);
 		//		console.log(val);
-				colorindex=parseInt(254*(val-minval)/(1.0*(maxval-minval)));
-				//console.log(colorindex);
-				var color=colormap[colorindex];
-				//console.log(color);
-				var s="rgb("+color[0]+","+color[1]+","+color[2]+")";
+				colorindex=parseInt(254*(val-tminval)/(1.0*(tmaxval-tminval)));
+				
+				//console.log(minval, maxval, colorindex);
+				color=colormap[colorindex];
+				
+				s="rgb("+color[0]+","+color[1]+","+color[2]+")";
 				//console.log('#r'+key+'_1',s);
-				el_ids=shape_ids[key];
-				for (i=0; i<el_ids.length; i++) {
-					el_id='#'+el_ids[i];				
-					el=$(el_id);
-					c=$(el).children();
-					for (j=0; j<c.length; j++){
-						c_j=c[j];
-						$(c_j).css('fill',s);
-						$(c_j).css('color',s);
-						}
-					}			
-			}
-		}
-	}
+				el_ids=shape_ids[regio];
+				
+				if (typeof(el_ids)!="undefined") {
+					
+					for (i=0; i<el_ids.length; i++) {
+						el_id='#'+el_ids[i];										
+						el=$(el_id);
+						c=$(el).children();						
+						 for (j=0; j<c.length; j++){
+							c_j=c[j];
+							$(c_j).css('fill',s);
+							$(c_j).css('color',s);
+							}	// for */
+						}		// for  
+					} // if typeof  
+					else {
+						console.log("undefined regio:", regio);
+					} 
+						
+			} /* if val!=0 */
+		} /* if row=daysel */
+	} /* for records */
+
+
 }
+
+
+
+function update_ts () {
+
+
+ canvas = d3.select("#ts")
+    		.append("svg")
+    		.attr('xmlns',"http://www.w3.org/2000/svg")
+    		.attr('id','svg_ts')            
+            .attr("width", ts_width)
+            .attr("height", ts_height);            
+	
+	var xScale=d3.time.scale();
+    xScale.domain([mindate,maxdate]);   // time in ms
+	xScale.range([50,ts_width]); 
+
+
+	console.log('update_ts:', regiosel, varsel);
+	var yScale=d3.scale.linear();
+    yScale.domain([maxval, minval]);
+	yScale.range([50,ts_height-50]); 
+	var line = d3.svg.line();
+
+	var xAxis=d3.svg.axis();
+    xAxis.scale(xScale)
+    	.orient("bottom");
+
+	var yAxis=d3.svg.axis();
+    yAxis.scale(yScale)
+    	.orient("left")   
+    	.tickFormat(function(d) {
+    			if ((d/1000)>=1) { d=d/1000+"k"; }
+    			return d;
+			});
+
+
+	var xGrid=d3.svg.axis();
+    xGrid.scale(xScale)
+    	.orient("bottom")
+    	.tickSize(-0.7*ts_height,0,0)
+    	.tickFormat(function(d) {
+    			return "";
+
+			});
+    		
+ 	var yGrid=d3.svg.axis();
+    yGrid.scale(yScale)
+    	.orient("left")
+    	.tickSize(-ts_width,0,0)
+    	.tickFormat(function(d) {
+    			return "";
+
+			});
+
+/* place axis & grids */
+
+   canvas.append("g")
+    		.attr("class","grid")
+    		.attr("transform","translate(0,"+(ts_height-50)+")")
+    		.call(xGrid);
+   canvas.append("g")
+    		.attr("class","grid")
+    		.attr("transform","translate(50,0)")
+			.call(yGrid);
+
+   canvas.append("g")
+    		.attr("class","xaxis")
+    		.attr("transform","translate(0,"+(ts_height-50)+")")
+    		.call(xAxis);
+   canvas.append("g")
+    		.attr("class","yaxis")
+    		.attr("transform","translate(50,0)")
+    		.call(yAxis);
+
+/* xas / yas labels */
+    canvas.append("text")
+    	.attr("class", "label")
+    	.attr("y", ts_height-10)
+    	.attr("x", ts_width/2)
+    	.text("tijd (uur)");
+
+    canvas.append("text")
+    	.attr("class", "label")
+    	.attr("y", ts_height/2)
+    	.attr("x", 10)
+    	.text("label");
+
+	
+
+var line=d3.svg.line()
+	.x(function(d,i)  { return xScale(xdata[i]); })
+	.y(function(d,i)  {  /*console.log(d,i, yScale(d));*/ return yScale(d); }); 
+
+	
+	xdata=[];
+	ydata=[];
+	regioreeks=regio_ts[regiosel];
+
+	for (i=0; i<regioreeks.length; i++) xdata.push(regioreeks[i][0]);  
+	for (i=0; i<regioreeks.length; i++) ydata.push(regioreeks[i][1]);  
+
+	console.log('daydata:',regioreeks[1]);
+	for (i=1; i<regioreeks.length; i++) {    		
+		canvas.append("svg:path")
+			//.attr("id","l_"+day+"_"+month)
+			.attr("class","dayl")
+			.attr("d", line(ydata))
+			.style("stroke","blue")
+			.style("fill","none")
+			.style("stroke-width","2")
+			.style("opacity","0.9");
+			
+	}
+
+
+}
+
+
+
+
+
+
 
 function setup_vars () {
 	var html='';
-	for (i=1; i<varnames.length; i++) {
+	var start=1;
+	if (has_date) start=2;
+	for (i=start; i<varnames.length; i++) {
 		var varname=varnames[i];
 		html+='<li class="varnameli"> <a href="#" id="v_'+varname+'_'+i+'"" class="varname">'+varname  + '</a></li>';
 	
 	}
+
 	$('#varlist').html(html);
 	$('.varname').on('click',change_var);
 }
 
+
+
 function init_svg(){
+	console.log('init_svg');
 	$('.outline').on('click',click_regio);
-	setup_vars();
-	colormap=colormap_hot(256);
+
+	setup_vars();	
+	update_var_info();
+	colormap=colormap_hot(256);	
 	//console.log(colormap);
+	console.log(minval,maxval,varsel);
+
+	if (has_date) {
+		prep_data();		
+		update_choropleth();
+		update_ts ();
+	}
+	
+
 }
 
 
---
+++ mapmaker_ts.py
@@ -0,0 +1,376 @@
+from shapely.geometry import Polygon
+from shapely.wkb import loads
+from osgeo import ogr, osr
+from matplotlib import pyplot
+from math import log, log10
+import sys, argparse, json
+
+import xml.etree.ElementTree as ET
+from StringIO import StringIO
+
+
+
+
+
+
+def drawpolygon(p,graph,colorval,regio_id):
+
+    # returns a list of dom-id's for every polygon drawn
+    # (multipolygons get
+    # dom-id format:  r%regioid_%counter
+    # 
+    bordercolor=(80/255.0,80/255.0,80/255.0)
+    borderwidth=0.5
+    
+    if not(hasattr(p,'geoms')):            
+        xList,yList = p.exterior.xy
+        h=graph.fill(xList,yList, color=colorval)
+        graph.plot(xList,yList, 
+                   color=bordercolor,
+                   linewidth=borderwidth)
+        for il, element in enumerate(h):
+            element.set_gid ("r%d_1" % regio_id)                        
+        return ["r%d_1" % regio_id]
+    else:
+        j=1
+        regs=[]
+        for poly in p:
+            xList,yList = poly.exterior.xy
+            h=graph.fill(xList,yList,color=colorval)
+            graph.plot(xList,yList,
+                        color=bordercolor,
+                        linewidth=borderwidth)            
+            for il, element in enumerate(h):
+                element.set_gid ("r%d_%d" % (regio_id,j) )   # polylines gaan niet goed            
+            regs.append("r%d_%d" % (regio_id, j))
+            j+=1            
+        return regs
+
+
+def rescale_color (val, minval, maxval):
+
+    minval=0;
+    maxval=log(maxval)    
+    if val is None:
+        return (1.0,1.0,1.0)
+    if val!=0:
+        val=log(val)
+        
+    if val<minval:
+        val=minval
+    if val>maxval:
+        val=maxval
+    val=(val-minval)/(1.0*(maxval-minval))   # range van 0 tot 1
+    
+  #  print 'ranged val:', val
+
+    colorval=(1-val,1-val,1)
+    colorval=(1,1-val,1-val)
+    return colorval
+    
+    
+
+def get_max_data(filename):
+
+    f=open(filename)
+    f.readline()
+    cols=f.readline().strip().split(',')
+    maxdata=int(cols[-1])
+    for line in f.readlines():
+        cols=line.strip().split(',')
+        val=int(cols[-1])
+        if val>maxdata:
+            maxdata=val
+    return maxdata
+
+
+
+def read_frame(f,varnames, prevline=None):
+    
+    datadict={}    
+    cols=prevline.strip().split(',')
+    prevcol=None
+    while prevcol is None or prevcol==cols[0]:        
+        prevcol=cols[0]
+        datadict[cols[1]]=int(cols[-1])
+        line=f.readline()
+        cols=line.strip().split(',')      
+        if len(cols)<=1:
+            return None, None, None
+  #  print datadict.items()
+    return datadict, prevcol, line
+
+
+def read_simple_frame(f, varnames):
+    datadict={}
+
+    for line in f.readlines():
+        cols=line.strip().split(',')
+        datadict[int(cols[0])]=int(cols[-1])
+        if len(cols)<=1:
+            return datadict
+    return datadict
+
+
+def prep_js (args):
+
+    csvfile=args['csvfile']
+    sep=args['sep']
+    outfile=args['outfile']
+    recordinfo=args['recordinfo']
+    recs=recordinfo.strip().split(',')
+    regiocol=recs.index('regio')
+    datecol=None
+    if 'date' in recs:
+        datecol=recs.index('date')
+    datacols = [i for i, x in enumerate(recs) if x == "data"]
+    
+    f=open (csvfile)
+
+    g=open("js/data.js",'w')    
+
+    varnames=f.readline().strip().split(sep)
+    s='var varnames=['
+    if datecol is not None:        
+        s+='"'+varnames[datecol]+'",'
+    s+='"'+varnames[regiocol]+'",'
+    s+=','.join(['"'+varnames[col]+'"' for col in datacols])+'];\n\n'
+    if datecol is not None:
+        s+='var has_date=true;\n'
+    s+='var data=[\n'
+    line_out=''
+    g.write(s)
+    
+    
+    # build js-object: [date], regio:{data1,data2, data3}],  ...
+    #
+    var_min=[None]*len(datacols)
+    var_max=[None]*len(datacols)
+    mindate=None
+    maxdate=None
+    line_out='\n'
+    for line in f.readlines():        
+        g.write(line_out)
+        cols=line.strip().split(sep)
+        line_out='['    
+        if (datecol is not None):
+            date=cols[datecol]
+            if mindate is None or date<mindate:
+                mindate=date
+            if maxdate is None or date>maxdate:
+                maxdate=date
+            line_out+=date+','            
+        line_out+=cols[regiocol]+','
+        line_out+=','.join([cols[col] for col in datacols])
+        line_out+='],\n'
+        #print line_out
+        for i,col in enumerate(datacols):
+            val=float(cols[col])
+            if var_min[i] is None or val<var_min[i]:
+                var_min[i]=val
+            if var_max[i] is None or val>var_max[i]:
+                var_max[i]=val
+            
+    var_min.insert(0,0)
+    var_max.insert(0,0)
+    if datecol is not None:
+        var_min.insert(0,int(mindate))         # min /max datum invoegen?
+        var_max.insert(0,int(maxdate))        
+    line_out=line_out[:-2]
+    g.write(line_out+'];\n')
+    f.close()
+
+    s=json.dumps(var_min)    
+    g.write('\n\nvar var_min='+s+';\n')
+    s=json.dumps(var_max)        
+    g.write('\n\nvar var_max='+s+';\n')
+    g.close()    
+
+
+
+
+
+def save_map (args, mapdata, layer):
+
+    fieldID=args['fieldID']
+    outfile=args['outfile']
+   # print mapdata
+    fig = pyplot.figure(figsize=(7, 8),dpi=300)    
+    ax = fig.add_subplot(1,1,1)    
+    nonecounter=0
+    regios=[]
+    regio_ids={}
+    for feature in layer:
+       # print feature.GetFieldCount()        
+        regio=int(feature.GetField(fieldID))        
+        val=mapdata.get(regio,None)
+        if val is None:
+            nonecounter+=1
+        colorval=rescale_color (val, 0, maxdata)
+       # print feature.DumpReadable()
+        #if val is not None:
+        #    print regio, val, colorval, val/(1.0*maxdata)
+      #  continue
+        geom=feature.GetGeometryRef()
+        if geom is not None:    
+            geometryParcel = loads(geom.ExportToWkb())
+            ids= drawpolygon(geometryParcel , ax, colorval, regio)    
+            regios=regios+ids;
+            regio_ids[regio]=ids
+    print 'saving img:%s (nones:%d)' % (outfile, nonecounter)
+    # add classes to DOM-objects
+    f = StringIO()
+    pyplot.savefig(f, format="svg")
+    tree, xmlid = ET.XMLID(f.getvalue())    
+    
+    
+    for r in regios:        
+        el = xmlid[r]  # lookup regio_id  in xml
+#        el.set('cursor', 'pointer')
+#        el.set('onclick', "toggle_hist(this)")
+        el.set('class', "outline")
+    ET.ElementTree(tree).write(outfile+'.svg')    
+
+    s=json.dumps(regio_ids);
+    f=open("js/shape_ids.js",'w')
+    f.write("var shape_ids=");
+    f.write(s);
+    f.write(';\n');
+    f.close()
+
+    
+
+
+
+
+def save_html (args):
+
+    outfile=args["outfile"]
+    fullhtml=args['fullhtml']
+    verbose=args['verbose']
+    
+    html=open ("map.html",'r').read()    
+    f=open(outfile+'.html','w')
+    
+    cssfrags=html.split('<link href="')
+    
+    f.write(cssfrags[0])
+    cssfiles=[cssfrag.split('"')[0] for cssfrag in cssfrags[1:]]                
+    for cssfrag in cssfrags[1:]:
+        cssfile=cssfrag.split('"')[0]
+        
+       # print cssfile
+        if fullhtml:
+            f.write('\n<style>\n')
+            css=open(cssfile,"r").read()
+            f.write(css)
+            f.write('\n</style>\n')
+            if verbose:
+                print cssfile,'included'
+        else:
+            s='<link href="'+cssfile+'" rel="stylesheet" type="text/css">'
+            f.write(s)
+            if verbose:
+                print cssfile,'skipped'
+            
+            
+
+    if fullhtml:
+        not_replaceable_js=[]
+    else:        
+        not_replaceable_js=['js-lib/jquery-2.0.3.min.js',
+                        'js-lib/d3.v3.min.js',
+                        'js-lib/chroma.min.js',
+                        'js/colormaps.js',                        
+                        'js/ui.js',                        
+                        'js/data.js',
+                        'js/shape_ids.js']
+    jsfrags=html.split('script src="')            
+    for jsfrag in jsfrags[1:]:
+        jsfile=jsfrag.split('"')[0]
+        if jsfile in not_replaceable_js:
+            s='\n<script type="text/javascript" src="'+jsfile+'"> </script>\n'
+            f.write(s)
+            if verbose:
+                print jsfile,'skipped'
+        else:
+            f.write('\n<script type="text/javascript>"\n')        
+            js=open(jsfile,'r').read()    
+            f.write(js)
+            f.write('\n</script>\n')
+            if verbose:
+                print jsfile,'included'
+        
+
+    body=html.split("<body>")
+    body=body[1]
+    svg=open(outfile+'.svg','r').read()                        
+    body=body.replace('<svg> </svg>',svg)
+    f.write("</head>\n")
+    f.write("<body>\n")
+    f.write(body)
+    f.close()
+    
+    
+
+
+
+
+
+    
+# Apparently, the `register_namespace` method works only with 
+# python 2.7 and up and is necessary to avoid garbling the XML name
+# space with ns0.
+ET.register_namespace("","http://www.w3.org/2000/svg")    
+
+
+parser = argparse.ArgumentParser(description='generate calendar from repeating data')
+
+parser.add_argument('-s', '--shapefile', dest='shapefile',  help='esri intput shapefile', required=False)
+parser.add_argument('-f', dest='fieldID',  help='shapefile area key variabele', required=False, default=',')                               
+parser.add_argument('-c', '--csvfile', dest='csvfile',  help='csv input file name', required=False)
+parser.add_argument('-d', dest='sep',  help='delimiter of csv infile', required=False, default=',')
+parser.add_argument('-o', dest='outfile',  help='output basename for .svg/.js', required=False, default='')
+parser.add_argument('-fullhtml', dest='fullhtml',  help='include everything (js, css) in html file', required=False, default=False, action='store_true')
+parser.add_argument('-verbose', dest='verbose',  help='verbose debuginfo', required=False, default=False, action='store_true')
+parser.add_argument('-r', '--record', dest='recordinfo',  help='recordbeschrijving: regiokey, data, regiolabel, dummy, key, keylabel', required=True)
+parser.add_argument('-title', dest='title',  help='title', required=False, default='')
+parser.add_argument('-label_x', dest='label_x',  help='title', required=False, default=0.1)
+parser.add_argument('-label_y', dest='label_y',  help='title', required=False, default=0.9)
+parser.add_argument('-label', dest='label',  help='title', required=False, default='label')
+
+args=vars(parser.parse_args())
+
+
+
+shapefile=args["shapefile"]
+csvfile=args["csvfile"]
+outfile=args["outfile"]
+fullhtml=args["fullhtml"]
+
+print ''
+
+driver = ogr.GetDriverByName('ESRI Shapefile')
+
+
+f=open (csvfile)
+varnames=f.readline().strip().split(',')
+maxdata=get_max_data(csvfile)
+
+
+
+
+
+line=f.readline()
+#mapdata, datum, line=read_frame(f,varnames,line)
+mapdata=read_simple_frame(f,varnames)
+
+#print mapdata.items()
+sh = ogr.Open(shapefile)
+layer = sh.GetLayer()
+
+prep_js(args)
+save_map (args, mapdata, layer)
+save_html (args)
+
+        
