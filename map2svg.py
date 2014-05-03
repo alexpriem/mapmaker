e89515ec67b809a5f1d1678528b4c73b91143c2a
Alex Priem <alexpriem@gmail.com>
2014-05-03 21:46:03 +0200

    added path-id's, proper bounding box, cleanups.

--- map2svg.py
+++ map2svg.py
@@ -1,42 +1,79 @@
 import shpUtils
-import matplotlib.pyplot as plt
 
-shpRecords = shpUtils.loadShapefile('f:\\data\\maps\\shapefiles\\gm_2012.shp')
+shpRecords = shpUtils.loadShapefile('shapefiles\\gm_2012.shp')
+regiocol='GM2012'
+classname='outline'
+outfile='test.svg'
 
-f=open ('test.svg','w')
+height=460
+width=390
+minx=None
+maxx=None
+miny=None
+maxy=None
 
-f.write ("""
-<svg xmlns="http://www.w3.org/2000/svg" height="460pt" version="1.1" viewBox="0 0 390 460" width="390pt">
- <defs>
-  <style type="text/css">
-*{stroke-linecap:butt;stroke-linejoin:round;}
-  </style>
- </defs>
- <g id="figure_1"> 
-""")
-         
 for i in range(0,len(shpRecords)):
     polygons=shpRecords[i]['shp_data']['parts']
-    for p in polygons:                
+    for poly in polygons:
+        for point in poly['points']:
+            tempx=float(point['x'])
+            tempy=float(point['y'])
+            if minx is None or tempx<minx:
+                minx=tempx
+            if miny is None or tempy<miny:
+                miny=tempy
+            if maxx is None or tempx>maxx:
+                maxx=tempx
+            if maxy is None or tempy>maxy:
+                maxy=tempy
+#print minx, maxx, miny, maxy
+dx=maxx-minx
+dy=maxy-miny
+
+
+
+
+f=open (outfile,'w')
+f.write ("""
+<svg xmlns="http://www.w3.org/2000/svg" height="%(height)spt" width="%(width)spt" viewBox="0 0 %(width)s %(height)s" version="1.1" >
+<defs>
+  <style type="text/css">
+  *{fill:#ff0000;stroke:#444444;}
+  </style>
+ </defs>
+ 
+<g id="figure_1"> 
+""" % locals ())
+
+
+
+if classname!='':
+    classtxt='class="'+classname+'"'
+for i in range(0,len(shpRecords)):
+    dbfdata=shpRecords[i]['dbf_data']
+    shape_id=dbfdata['GM2012']    
+    polygons=shpRecords[i]['shp_data']['parts']
+    for shape_nr, poly in enumerate(polygons): 
+        
         x = []
         y = []
-        s='<path class="outline" d=" '
-        tempx = float(p['points'][0]['x'])/1000.0
-        tempy = 300.0-(float(p['points'][0]['y'])/1000.0-300.0)
+        s='<path %s id="%d_%d" data-shapeid="%d" d=" ' % (classtxt,shape_id, shape_nr, shape_id)
+        point=poly['points'][0]        
+        tempx = ((float(point['x'])-minx)/dx)*width
+        tempy = height - ((float(point['y'])-miny)/dy)*height
         s+='M %.2f %.2f' % (tempx, tempy)
-        for j in range(1,len(p['points'])):                    
-            tempx = float(p['points'][j]['x'])/1000.0
-            tempy = 300.0- (float(p['points'][j]['y'])/1000.0-300.0)
+        for point in poly['points']:
+            tempx = ((float(point['x'])-minx)/dx)*width
+            tempy = height - ((float(point['y'])-miny)/dy)*height
             x.append(tempx)
             y.append(tempy)
             
             s+='L%.2f %.2f' % (tempx,tempy)
         #plt.fill(x,y, color='red', linecolor='black')
-        s+=' z "   '
-        s+='style="fill:#ff0000;stroke:#444444;" />\n'
+        s+=' z " />\n  '
+        #s+='style="fill:#ff0000;stroke:#444444;" />\n'
 
         f.write(s)
-        plt.plot(x,y, color='black')
 
 
 f.write("""
@@ -44,8 +81,5 @@ f.write("""
 </svg>
 """)
         
-plt.axis('equal')
-plt.title("Testing")
-plt.show()
 
 
